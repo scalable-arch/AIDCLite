3ecaf5c417c2d4640d87ff864a26d3393392a17b
Jungrae Kim <dale40@gmail.com>
2023-07-07 13:08:48 +0900

    lint

--- design/sverilog/filelist.f
+++
@@ -1,5 +0,0 @@
--sverilog
-+incdir+${AIDC_LITE_DIR}/design/sverilog
-${AIDC_LITE_DIR}/design/sverilog/AIDC_LITE_COMP_CFG.sv
-${AIDC_LITE_DIR}/design/sverilog/AIDC_LITE_COMP_ENGINE.sv
-${AIDC_LITE_DIR}/design/sverilog/AIDC_LITE_COMP_TOP.sv
---
+++ filelist.design.f
@@ -0,0 +1,5 @@
+-sverilog
++incdir+${AIDC_LITE_HOME}/design/sverilog
+${AIDC_LITE_HOME}/design/sverilog/AIDC_LITE_COMP_CFG.sv
+${AIDC_LITE_HOME}/design/sverilog/AIDC_LITE_COMP_ENGINE.sv
+${AIDC_LITE_HOME}/design/sverilog/AIDC_LITE_COMP_TOP.sv
