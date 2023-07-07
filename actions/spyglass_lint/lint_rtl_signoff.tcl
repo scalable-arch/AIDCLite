e0085a53eb6ea6f0ef1d8fbae3e9c031e3533649
Jungrae Kim <dale40@gmail.com>
2023-07-07 16:00:00 +0900

    working

--- actions/spyglass_lint/lint_rtl_signoff.tcl
+++ actions/spyglass_lint/lint_rtl_signoff.tcl
@@ -1,19 +1,31 @@
 ############################################################
-# check inputs
+# check environment variables
 ############################################################
-if { ![info exists $env(DESIGN_TOP)] } {
-    puts "\$DESIGN_TOP env variable is not set"        
-    return 1;   // test failed        
-}
-if { ![info exists $env(DESIGN_FILELIST)] } {
-    puts "\$DESIGN_FILELIST env variable is not set"        
+if { [info exists env(SPYGLASS_HOME)] } {
+    puts "\$SPYGLASS_HOME: $env(SPYGLASS_HOME)";        
+} else {
+    puts "(Env setting error): \$SPYGLASS_HOME is not set"        
     return 1;   // test failed        
 }
 
-set project RTL_HANDOFF.$DESIGN_TOP
+if { [info exists env(DESIGN_TOP)] } {
+    puts "\$DESIGN_TOP: $env(DESIGN_TOP)";        
+} else {
+    puts "(IP setting error): \$DESIGN_TOP is not set"        
+    return 1;   // test failed        
+}
+
+if { [info exists env(DESIGN_FILELIST)] } {
+    puts "\$DESIGN_FILELIST: $env(DESIGN_FILELIST)";        
+} else {
+    puts "(IP setting error): \$DESIGN_FILELIST is not set"        
+    return 1;   // test failed        
+}
+
+set project RTL_HANDOFF.$env(DESIGN_TOP)
 
 # Delete an existing project, if any
-file delete -force -- ${project}
+file delete -force -- ${project}.prj ${project}
 
 # Create a new project
 new_project ${project}
@@ -22,7 +34,7 @@ new_project ${project}
 current_methodology $env(SPYGLASS_HOME)/GuideWare/latest/block/rtl_handoff
 
 #Read Files
-read_file -type sourcelist $DESIGN_FILELIST
+read_file -type sourcelist $env(DESIGN_FILELIST)
 
 #Read waiver files
 #read_file -type awl ../waiver.awl
